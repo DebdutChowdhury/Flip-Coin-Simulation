81d043d20729404e62ee772c8cef0c9197ce2144
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-01-14 20:44:33 +0530

    Added: All value store in dictionary and display with percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,3 +1,43 @@
 #!/bin/bash
 
-# Welcome to my Flip Coin Simulation world !!
+read -p "Enter the number: " num
+
+declare -A flipCoin
+
+for (( i=0; i<num; i++ ))
+do
+        rand=$((RANDOM%2))
+        if [ $rand -eq 1 ]
+        then
+                flipCoin[$i]="Head"
+        else
+                flipCoin[$i]="Tail"
+        fi
+done
+
+echo keys: ${!flipCoin[@]}
+echo values: ${flipCoin[@]}
+
+heads=0
+tails=0
+
+for j in ${flipCoin[@]}
+do
+        if [ "$j" == "Head" ]
+        then
+                ((heads++))
+        elif [ "$j" == "Tail" ]
+        then
+                ((tails++))
+        else
+                echo Invallid 
+        fi
+done
+echo Heads: $heads
+echo Tails: $tails
+
+per_head=$(((heads*100)/num))
+per_tail=$(((tails*100)/num))
+
+echo Singlet percentage of heads: $per_head %
+echo Singlet percentage of tails: $per_tail %
