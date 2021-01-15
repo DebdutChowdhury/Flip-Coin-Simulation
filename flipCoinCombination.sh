ef8ef7836c65951c5c6f35c8dae93f241c946a20
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-01-15 11:17:11 +0530

    Added: Triplet flipcoin to Display heads or tails and there percentages

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -6,56 +6,96 @@ declare -A flipCoin
 
 for (( i=0; i<num; i++ ))
 do
-        rand=$((RANDOM%4))
+        rand=$((RANDOM%6))
         if [ $rand -eq 1 ]
         then
-                flipCoin[$i]="Hh"
+                flipCoin[$i]="Hhh"
 	elif [ $rand -eq 2 ]
 	then
-		flipCoin[$i]="Tt"
+		flipCoin[$i]="Hht"
 	elif [ $rand -eq 3 ]
 	then
-		flipCoin[$i]="Ht"
+		flipCoin[$i]="Thh"
+	elif [ $rand -eq 4 ]
+	then 
+		flipCoin[$i]="Tth"
+	elif [ $rand -eq 5 ]
+	then
+		flipCoin[$i]="Htt"
+	elif [ $rand -eq 6 ]
+	then 
+		flipCoin[$i]="Hth"
+	elif [ $rand -eq 5 ]
+	then
+		flipCoin[$i]="Tth"
         else
-                flipCoin[$i]="Th"
+                flipCoin[$i]="Ttt"
         fi
 done
 
 echo keys: ${!flipCoin[@]}
 echo values: ${flipCoin[@]}
 
-hh=0
-ht=0
-th=0
-tt=0
+hhh=0
+hht=0
+thh=0
+tth=0
+htt=0
+hth=0
+tht=0
+ttt=0
 
 
 for j in ${flipCoin[@]}
 do
-        if [ "$j" == "Hh" ]
+        if [ "$j" == "Hhh" ]
         then
-                ((hh++))
-        elif [ "$j" == "Tt" ]
+                ((hhh++))
+        elif [ "$j" == "Hht" ]
         then
-                ((tt++))
-	elif [ "$j" == "Ht" ]
+                ((hht++))
+	elif [ "$j" == "Thh" ]
 	then
-		((ht++))
+		((thh++))
+	elif [ "$j" == "Tth" ]
+	then
+		((tth++))
+	elif [ "$j" == "Htt" ]
+	then
+		((htt++))
+	elif [ "$j" == "Hth" ]
+	then
+		((hth++))
+	elif [ "$j" == "Tht" ]
+	then
+		((tht++))
 	else
-		((th++))
+		((ttt++))
         fi
 done
-echo Heads: $hh
-echo Tails: $tt
-echo Combo: $th
-echo Combo: $ht
+echo Heads: $hhh
+echo Tails: $ttt
+echo Combo: $hht
+echo Combo: $thh
+echo combo: $tth
+echo combo: $htt
+echo combo: $hth
+echo combo: $tht
 
-per_hh=$(((hh*100)/num))
-per_tt=$(((tt*100)/num))
-per_th=$(((th*100)/num))
-per_ht=$(((ht*100)/num))
-echo Singlet percentage of heads: $per_hh %
-echo Singlet percentage of tails: $per_tt %
-echo Singlet percentage of combo: $per_th %
-echo Singlet percentage of combo: $per_ht %
+per_hhh=$(((hhh*100)/num))
+per_ttt=$(((ttt*100)/num))
+per_thh=$(((thh*100)/num))
+per_htt=$(((htt*100)/num))
+per_hht=$(((hht*100)/num))
+per_tth=$(((tth*100)/num))
+per_hth=$(((hth*100)/num))
+per_tht=$(((tht*100)/num))
 
+echo Singlet percentage of heads: $per_hhh %
+echo Singlet percentage of tails: $per_ttt %
+echo Singlet percentage of combo: $per_thh %
+echo Singlet percentage of combo: $per_htt %
+echo Singlet percentage of combo: $per_hht %
+echo Singlet percentage of combo: $per_tth %
+echo Singlet percentage of combo: $per_hth %
+echo Singlet percentage of combo: $per_tht %
